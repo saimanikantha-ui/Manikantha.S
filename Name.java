0209f7b2d39ac24013cb8c7d5304ae04234b493d
saimanikantha-ui <59251348+saimanikantha-ui@users.noreply.github.com>
2019-12-26 15:59:15 +0530

    Add files via upload

---
+++ Name.java
@@ -0,0 +1,20 @@
+package hello;
+
+public class Name {
+
+	public static void main(String[] args) { int loop; //loop counter declaration
+    final String name="SAI MANIKANTHA"; //name as constant
+    
+    loop=1; //initialization of loop counter
+    while(loop<=27){
+        System.out.println(name);
+        loop++; //increment
+    }
+     
+}
+
+		// TODO Auto-generated method stub
+
+	}
+
+
